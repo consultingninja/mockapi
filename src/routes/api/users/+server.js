c6a2c1467af623822f77e7716f6020e000f4046b
consultingninja <consultingninja.tech@gmail.com>
2024-03-14 09:25:01 -0500

    add users get route

---
+++ src/routes/api/users/+server.js
@@ -0,0 +1,14 @@
+
+
+const users = [
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Smith' },
+  { id: 3, name: 'Bob Johnson' },
+  { id: 4, name: 'Alice Williams' },
+  { id: 5, name: 'Mike Brown' },
+];
+
+
+export const GET = async ({}) =>{
+  return new Response(JSON.stringify(users),{status: 200})
+}
